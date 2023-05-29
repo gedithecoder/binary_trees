5ce8ad78ee503ed515bbcc9d801573aaca2a30cc
gedithecoder <117764141+gedithecoder@users.noreply.github.com>
2023-05-29 15:04:30 +0300

    Update 5-binary_tree_is_root.c

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - check if node is root
+ * @node: node of tree
+ *
+ * Return: 1 if root, 0 if not
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+if (node && !node->parent)
+return (1);
+return (0);
+}
